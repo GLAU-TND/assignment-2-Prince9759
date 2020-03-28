7a818d0e9d8a9981b289c2e36379402874cad784
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-28 18:53:40 +0530

    create getter and setter

--- src/problem3/Student/student.java
+++ src/problem3/Student/student.java
@@ -8,4 +8,20 @@ public class student {
         this.name = name;
         this.rollno = rollno;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
 }

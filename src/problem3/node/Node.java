dc9e515cbb55ab6ede9b47367644be78282ab7e5
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-28 18:56:20 +0530

    create getter and setter

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -12,6 +12,22 @@ public class Node {
     private student data;
     private Node next;
 
+    public student getData() {
+        return data;
+    }
+
+    public void setData(student data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
     public Node(student data) {
         this.data = data;
         this.next=next;

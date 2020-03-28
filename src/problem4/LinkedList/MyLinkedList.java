abc06ebdef55ffd2fcf91f47bc242ea2cdd1e46f
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-28 19:17:43 +0530

    create addLast

--- src/problem4/LinkedList/MyLinkedList.java
+++ src/problem4/LinkedList/MyLinkedList.java
@@ -1,4 +1,16 @@
 package problem4.LinkedList;
 
 public class MyLinkedList {
-}
+    private Node first;
+    private Node last;
+    public void addLast(int data){
+        Node node=new Node(data);
+        if(first==null){
+            first=last=null;
+            return;
+        }
+        last.setNext(node);
+        last=node;
+    }
+    }
+

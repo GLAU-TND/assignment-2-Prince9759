9c07195eab6081d4880b606c722f760c82a3d6f3
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-28 18:27:36 +0530

    create isFull and isEmpty method

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,21 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.student.Student;
+
 //to implement circular queue
 public class MyCircularQueue {
+    private int size;
+    private int beginning;
+    private int top;
+    private int maxSize;
+    private Student[] circularQueue;
+    public boolean isEmpty(){
+        return size==0;
+    };
+
+    private boolean isFull(){
+        return size==maxSize;
+    }
 }

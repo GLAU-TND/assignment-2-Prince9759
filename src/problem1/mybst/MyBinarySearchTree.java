35e3c38c188da01f38e1199e5b131a8942eca403
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-03-28 18:12:27 +0530

    create getter and setter

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -37,4 +37,12 @@ public class MyBinarySearchTree {
             }
         }
     }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
 }
